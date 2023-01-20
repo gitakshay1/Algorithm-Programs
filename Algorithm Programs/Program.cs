4f0da7e14615cfb3e3920d96b8a178a1995e31b4
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-20 18:15:33 +0530

    [Akshay] added program for Permutation of string

---
+++ Algorithm Programs/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithm_Programs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Programs!");
+            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("Enter the option");
+            int num = Convert.ToInt32(Console.ReadLine());
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Enter String to find Permutation ");
+                    string str = Console.ReadLine();
+                    int n = str.Length;
+                    Permutations.Permutate(str, 0, n - 1);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the valid option!");
+                    break;
+            }
+            
+    }
+}
