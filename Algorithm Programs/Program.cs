e455f842a0d706baf7a54ff4fdf7cfa6ee349659
gitakshay1 <akshaypatil309p8@gmail.com>
2023-01-20 19:09:04 +0530

    [Akshay] Added program for binary search the word from wordlist

--- Algorithm Programs/Program.cs
+++ Algorithm Programs/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Algorithm Programs!");
-            Console.WriteLine("1.Permutation Using Recursion");
+            Console.WriteLine("1 Permutation Using Recursion\n2 Binary Search the word from wordlist");
             Console.WriteLine("Enter the option");
             int num = Convert.ToInt32(Console.ReadLine());
             switch (num)
@@ -16,11 +16,17 @@
                     int n = str.Length;
                     Permutations.Permutate(str, 0, n - 1);
                     break;
+                case 2:
+                    string txtpath = File.ReadAllText(@"C:\Users\aksha\Assignments\Algorithm-Programs\Algorithm Programs\TextFile1.txt");
+                    List<string> words = new List<string>(txtpath.Split());
+                    words.Sort();
+                    BinarySearch.BinarySearchWord(words);
+                    break;
 
                 default:
                     Console.WriteLine("Enter the valid option!");
                     break;
             }
-            
+        }            
     }
 }
